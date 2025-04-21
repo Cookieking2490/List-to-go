6e2b00465bea2b398c3f1e94b42bac5904c274c0
Abdelrahman Elsayed <61527085+Jayz2@users.noreply.github.com>
2025-04-21 17:54:08 +0200

    tasks

---
+++ Server (Backend)/Code/todo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from .models import Task
+from datetime import date
+
+
+# @login_required 
+def create_task(request):
+    form = TaskForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        task = form.save(commit=False)
+        # task.user = request.user  # assign the logged-in user
+        task.save()
+        return redirect('task_list')
+    return render(request, 'todo/create_task.html', {'form': form})
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/edit_task.html', {'form': form, 'task': task})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'todo/delete_task.html', {'task': task})
