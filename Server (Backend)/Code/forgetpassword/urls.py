024140775a58d8054db6c6cb581e4b97c7a77af3
Cookieking2490 <151520727+Cookieking2490@users.noreply.github.com>
2025-04-24 23:17:07 +0200

    Revert "Merge pull request #8 from Cookieking2490/Abdelrahman"
    This reverts commit 121aee74cd45b8cb5428c187f5ee8780bb35c46c, reversing
    changes made to 2324497fc8a2fa43bf7c3759f92199e61fab9738.

--- Server (Backend)/Code/forgetpassword/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import send_reset_code, verify_reset_code, reset_password
-
-urlpatterns = [
-    path("send-reset-code/", send_reset_code, name="send-reset-code"),
-    path("verify-reset-code/", verify_reset_code, name="verify-reset-code"),
-    path("reset-password/", reset_password, name="reset-password"),
-]
--- Server (Backend)/Code/tasks/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import search_tasks
-
-urlpatterns=[
-    path('search/',search_tasks,name="search_tasks"),
-]
